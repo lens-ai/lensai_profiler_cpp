cb3e448bf12a9b021255ea6392d9d03f5db5ecf2
Venkata Pydipalli <vsnm.tej@gmail.com>
2024-07-15 22:49:42 +0200

    Added error handiling for create folder generic method.

--- src/helpers/generic.cpp
+++ src/helpers/generic.cpp
@@ -1,16 +1,23 @@
 #include <filesystem>
+#include <iostream>
 
 bool createFolderIfNotExists(const std::string& statSavepath, const std::string& dataSavepath) {
-  try {
-    if (!std::filesystem::exists(statSavepath)) {
-      std::filesystem::create_directories(statSavepath);
-      }
-    if(!std::filesystem::exists(dataSavepath)) {
-      std::filesystem::create_directories(dataSavepath);		    
-      }
+    try {
+        if (!std::filesystem::exists(statSavepath)) {
+            std::filesystem::create_directories(statSavepath);
+        }
     } catch (const std::filesystem::filesystem_error& e) {
-      // Handle other filesystem errors (optional)
-      return false;
+        std::cerr << "Error creating statSavepath: " << e.what() << std::endl;
+        return false;
     }
-  return true;
+
+    try {
+        if (!std::filesystem::exists(dataSavepath)) {
+            std::filesystem::create_directories(dataSavepath);
+        } 
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Error creating dataSavepath: " << e.what() << std::endl;
+        return false;
+    }
+    return true;
 }
